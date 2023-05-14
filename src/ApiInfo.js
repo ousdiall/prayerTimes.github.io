8f8b740a0a2490ce15993135dde3099eda2a5738
Ousmane Diall <tun47370@temple.edu>
2023-05-14 11:02:12 -0400

    Modified index.js

--- src/ApiInfo.js
+++ src/ApiInfo.js
@@ -1,5 +1,5 @@
 export const api = {
-        base: 'http://api.aladhan.com/v1/calendarByCity/',
+        base: 'https://api.aladhan.com/v1/calendarByCity/',
         latLon: 'https://api.weatherapi.com/v1/forecast.json',
         year: new Date().getFullYear(),
         month: new Date().getMonth() + 1,
